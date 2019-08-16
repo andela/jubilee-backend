159eb0d69fa06c1d11bf84b668da8b3700487529
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-08-16 10:03:21 +0100

    fix(chaiHttp): remove unnecessary chaiHttp

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,6 @@
 import chai from 'chai';
-import chaiHttp from 'chai-http';
 
 const { expect }  = chai;
-chai.use(chaiHttp);
 
 describe('Initial test', () => {
   it('should be equal to barefoot', () => {
