060f68cdac7207dc5e5115a5f73a6fee39aec9d6
Emmanuel Nwankwo <cnwankwom@gmail.com>
2019-08-16 11:17:47 +0100

    fix(eslint):resolve eslint errors

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,22 +4,22 @@ import swaggerDocument from '../../../swagger.json';
 
 const router = Router();
 
-router.use("/", require("./users"));
+router.use('/', require('./users'));
 
-router.use(function(err, req, res, next) {
-    if (err.name === "ValidationError") {
-        return res.status(422).json({
-            errors: Object.keys(err.errors).reduce(function(errors, key) {
-                errors[key] = err.errors[key].message;
-                return errors;
-            }, {})
-        });
-    }
+router.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce((errors, key) => {
+        errors[key] = err.errors[key].message;
+        return errors;
+      }, {})
+    });
+  }
 
-    return next(err);
+  return next(err);
 });
 
-router.use("/docs", swaggerUi.serve);
-router.get("/docs", swaggerUi.setup(swaggerDocument));
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
