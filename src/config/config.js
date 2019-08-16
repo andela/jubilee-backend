187dd390218067636564c64eaf45b5b53832ae05
cloud <ejimchisom@gmail.com>
2019-08-16 17:06:08 +0100

    chore(sequelize): setup sequelize

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  production: {
+    use_env_variable: process.env.DATABASE_URL
+  },
+
+  development: {
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DATABASE_DEV,
+    host: process.env.DATABASE_URL_DEV,
+    port: process.env.PORT || 5432,
+    dialect: 'postgres'
+  },
+
+  test: {
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_DATABASE_TES,
+    host: process.env.DATABASE_URL_TEST,
+    port: process.env.PORT || 5432,
+    dialect: 'postgres'
+  }
+};
