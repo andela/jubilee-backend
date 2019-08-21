bad52a3a1babbd99242d53ca04ead8cbca3233eb
The Stoic Developer <code4kings@gmail.com>
2019-08-22 00:13:40 +0100

    refactor(server): remove redundant code and handle invalid routes

--- src/index.js
+++ src/index.js
@@ -2,60 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
-import errorhandler from 'errorhandler';
 import routes from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// routes
 app.use(routes);
+app.get('/', (req, res) => res.status(200).send({ message: 'welcome to BN: jubilee-team' }));
+app.all('*', (req, res) => res.send({ message: 'route not found' }));
 
-if (!isProduction) {
-  app.use(errorhandler());
-}
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-  app.use((err, req, res) => {
-    console.log(err.stack);
-    res.status(err.status || 500);
-    res.json({
-      errors: {
-        message: err.message,
-        error: err
-      }
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.json({
-    errors: {
-      message: err.message,
-      error: {}
-    }
-  });
-});
-
-// finally, let's start our server...
 app.listen(port, () => {
   console.info(`Listening on port ${port}`);
 });
+
+export default app;
