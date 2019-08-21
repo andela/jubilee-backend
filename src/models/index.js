270db0c57c304dc9f7d12dafbd7acd8e29753040
The Stoic Developer <code4kings@gmail.com>
2019-08-21 23:24:30 +0100

    chore(user table): refactor model/index file to use es6 instead and ensure Users table gets created upon migration

--- src/models/index.js
+++ src/models/index.js
@@ -1,31 +1,36 @@
-'use strict';
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+import sequelizeConfig from '../config/config';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+dotenv.config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = sequelizeConfig[env];
 const db = {};
-
 let sequelize;
 if (config.prodDatabaseURI) {
   sequelize = new Sequelize(process.env[config.prodDatabaseURI], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+fs.readdirSync(__dirname)
+  .filter(
+    (file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+  )
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
