92c521fce08c63a4c2afc1d052013a4199a5343e
Segun Ogundipe <davephenoms@gmail.com>
2019-08-22 01:59:24 +0100

    chore(ApiError):add ApiError for reporting errors

---
+++ src/services/UserService.js
@@ -0,0 +1,7 @@
+export default class UserService {
+    static async createUser() {
+        try {
+
+        }
+    }
+}
---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+/**
+ * Error class for Barefoot Nomad
+ * @extends Error
+ */
+export default class ApiError extends Error {
+  /**
+   * Create an instance of ApiError
+   * @param {string} status - The status code of the error
+   * @param {string} message - The error message
+   */
+  constructor(status, message) {
+    super();
+
+    this.status = status;
+    this.message = message;
+  }
+}
