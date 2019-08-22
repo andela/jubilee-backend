f3c674b3893f278fc8c2ddd77612ed75bbb184cf
The Stoic Developer <code4kings@gmail.com>
2019-08-22 18:55:39 +0100

    refactor(config): add fallback host to db config

--- src/config/config.js
+++ src/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     username: process.env.DB_USERNAME_DEV,
     password: process.env.DB_PASSWORD_DEV,
     database: process.env.DATABASE_DEV,
-    host: process.env.DATABASE_URL_DEV,
+    host: process.env.DATABASE_URL_DEV || '127.0.0.1',
     port: process.env.DB_PORT_DEV || 5432,
     dialect: 'postgres'
   },
@@ -21,7 +21,7 @@ module.exports = {
     username: process.env.DB_USERNAME_TEST,
     password: process.env.DB_PASSWORD_TEST,
     database: process.env.DATABASE_TEST,
-    host: process.env.DATABASE_URL_TEST,
+    host: process.env.DATABASE_URL_TEST || '127.0.0.1',
     port: process.env.DB_PORT_TEST || 5432,
     dialect: 'postgres'
   }
