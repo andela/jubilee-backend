dde5b2022e415eb66312ba823a8f8867c8b0a44f
Segun Ogundipe <davephenoms@gmail.com>
2019-08-22 13:35:21 +0100

    chore(service):update user service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,4 @@
 import { hashSync, genSaltSync } from 'bcryptjs';
-import ApiError from '../utils/index';
 import database from '../models';
 
 /**
@@ -12,12 +11,8 @@ export default class UserService {
    * @param {object} user - The user to be saved in the database
    * @returns {Promise<object>} A promise object with user detail.
    */
-  static async create(user) {
-    try {
-      user.password = hashSync(user.password, genSaltSync(10));
-      return await database.User.create(user);
-    } catch (error) {
-      throw new ApiError(500, error.message);
-    }
+  static create(user) {
+    user.password = hashSync(user.password, genSaltSync(10));
+    return database.User.create(user);
   }
 }
