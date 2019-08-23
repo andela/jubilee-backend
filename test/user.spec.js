ed6237715f22a420098d13cac53e159d3b0023eb
Segun Ogundipe <davephenoms@gmail.com>
2019-08-23 09:01:18 +0100

    chore(test):log response to get error context

--- test/user.spec.js
+++ test/user.spec.js
@@ -39,6 +39,7 @@ describe('Auth route', () => {
       };
 
       const response = await request.post('/api/auth/signup').send(user);
+      console.log(response.body);
       expect(response.body.status).to.equal(201);
       expect(response.body.data).to.be.a('object');
       expect(response.body.data.token).to.be.a('string');
