66dcd13d2a857a2498b4d3aecb860ed97d788b95
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-08-23 08:59:28 +0100

    test(login): add test for login route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -5,21 +5,20 @@ import userRouter from './users';
 
 const router = Router();
 
-// Auth routes
 router.use('/', userRouter);
 
-router.use((err, req, res, next) => {
-  if (err.name === 'ValidationError') {
-    return res.status(422).json({
-      errors: Object.keys(err.errors).reduce((errors, key) => {
-        errors[key] = err.errors[key].message;
-        return errors;
-      }, {})
-    });
-  }
+// router.use((err, req, res, next) => {
+//   if (err.name === 'ValidationError') {
+//     return res.status(422).json({
+//       errors: Object.keys(err.errors).reduce((errors, key) => {
+//         errors[key] = err.errors[key].message;
+//         return errors;
+//       }, {})
+//     });
+//   }
 
-  return next(err);
-});
+//   return next(err);
+// });
 
 router.use('/docs', swaggerUi.serve);
 router.get('/docs', swaggerUi.setup(swaggerDocument));
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -23,39 +23,39 @@ export default class UserService {
  * @param {object} res response object
  * @returns {object}  JSON response
  */
-static async userLogin(userDetails, res) {
-  const { email, password } = userDetails;
-  if (!email || !password) {
-    return res.status(400).json({
-      status: 400,
-      error: 'Please provide complete login details'
-    });
-  }
-  try {
-    const user = await database.User.findOne({
-      where: { email }
-    });
-    if (!user) {
-      return res.status(401).json({
-        status: 401,
-        error: 'Invalid login details',
+  static async userLogin(userDetails, res) {
+    const { email, password } = userDetails;
+    if (!email || !password) {
+      return res.status(400).json({
+        status: 400,
+        error: 'Please provide complete login details'
       });
     }
-    // if (!Auth.compare(password, user.password)) {
-    //   return res.status(401).json({
-    //     status: 401,
-    //     error: 'Invalid login details',
-    //   });
-    // }
-    const { id, firstName, lastName } = user;
-    const output = { email, firstName, lastName };
-    const token = Auth.generateToken({ id, email });
-    return res.status(200).json({
-      status: 200,
-      data: { token, ...output }
-    });
-  } catch (error) {
-    return error;
+    try {
+      const user = await database.User.findOne({
+        where: { email }
+      });
+      if (!user) {
+        return res.status(401).json({
+          status: 401,
+          error: 'Invalid login details',
+        });
+      }
+      if (!Auth.compare(password, user.password)) {
+        return res.status(401).json({
+          status: 401,
+          error: 'Invalid password',
+        });
+      }
+      const { id, firstName, lastName } = user;
+      const output = { email, firstName, lastName };
+      const token = Auth.generateToken({ id, email });
+      return res.status(200).json({
+        status: 200,
+        data: { token, ...output }
+      });
+    } catch (error) {
+      return error;
+    }
   }
-}
 }
