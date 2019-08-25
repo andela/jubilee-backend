ceaf4fbdfe8cde36519d7fd8913d82dc72a6adfe
Ayodele <daylay92@yahoo.com>
2019-08-25 19:04:45 +0100

    feature(modify): refactor unit tests [Finishes #167733643]

--- src/migrations/20190822144509-create-user.js
+++ src/migrations/20190822144509-create-user.js
@@ -1,87 +1,83 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      birthDate: {
-        type: Sequelize.DATE
-      },
-      preferredLanguage: {
-        type: Sequelize.STRING
-      },
-      preferredCurrency: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      gender: {
-        type: Sequelize.STRING
-      },
-      street: {
-        type: Sequelize.STRING
-      },
-      city: {
-        type: Sequelize.STRING
-      },
-      state: {
-        type: Sequelize.STRING
-      },
-      country: {
-        type: Sequelize.STRING
-      },
-      zip: {
-        type: Sequelize.STRING
-      },
-      phoneNumber: {
-        type: Sequelize.STRING
-      },
-      companyName: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      role: {
-        type: Sequelize.STRING
-      },
-      isVerified: {
-        type: Sequelize.BOOLEAN
-      },
-      facebookId: {
-        type: Sequelize.STRING
-      },
-      googleId: {
-        type: Sequelize.STRING
-      },
-      department: {
-        type: Sequelize.STRING
-      },
-      lineManager: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    },
+    birthDate: {
+      type: Sequelize.DATE
+    },
+    preferredLanguage: {
+      type: Sequelize.STRING
+    },
+    preferredCurrency: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    gender: {
+      type: Sequelize.STRING
+    },
+    street: {
+      type: Sequelize.STRING
+    },
+    city: {
+      type: Sequelize.STRING
+    },
+    state: {
+      type: Sequelize.STRING
+    },
+    country: {
+      type: Sequelize.STRING
+    },
+    zip: {
+      type: Sequelize.STRING
+    },
+    phoneNumber: {
+      type: Sequelize.STRING
+    },
+    companyName: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    role: {
+      type: Sequelize.STRING
+    },
+    isVerified: {
+      type: Sequelize.BOOLEAN
+    },
+    facebookId: {
+      type: Sequelize.STRING
+    },
+    googleId: {
+      type: Sequelize.STRING
+    },
+    department: {
+      type: Sequelize.STRING
+    },
+    lineManager: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users')
 };
--- test/auth.spec.js
+++ test/auth.spec.js
@@ -10,7 +10,7 @@ const { generateToken } = Helpers;
 chai.use(chaiHttp);
 
 let newlyCreatedUser = {};
-describe('Auth Route Endpoisignup', () => {
+describe('Auth route endpoints', () => {
   it('should signup successfully with a status of 201', async () => {
     const user = {
       email: faker.internet.email(),
@@ -37,98 +37,55 @@ describe('Auth Route Endpoisignup', () => {
     expect(response.body.data.firstName).to.be.a('string');
     expect(response.body.data.lastName).to.be.a('string');
   });
-  it("should send a verification link to a user's email upon successful registration", (done) => {
-    chai
-      .request(server)
-      .post('/api/auth/signup')
-      .send(newUser)
-      .end((err, res) => {
-        const {
-          body: { data }
-        } = res;
-        newlyCreatedUser = { ...data };
-        expect(res).to.have.status(201);
-        expect(data.isVerified).to.equal(false);
-        expect(data.emailSent).to.equal(true);
-        done(err);
-      });
+  it("should send a verification link to a user's email upon successful registration", async () => {
+    const response = await chai.request(server).post('/api/auth/signup').send(newUser);
+    const { body: { data } } = response;
+    newlyCreatedUser = { ...data };
+    expect(response).to.have.status(201);
+    expect(data.isVerified).to.equal(false);
+    expect(data.emailSent).to.equal(true);
   });
-  it('should return a response that indicates verification link was not sent whenever a mail is not sent upon successful registration', (done) => {
+
+  it('should return a response that indicates verification link was not sent whenever a mail is not sent upon successful registration', async () => {
     const user2 = { ...newUser };
     user2.email = 'notvalid';
-    chai
-      .request(server)
-      .post('/api/auth/signup')
-      .send(user2)
-      .end((err, res) => {
-        const {
-          body: {
-            data: { isVerified, emailSent }
-          }
-        } = res;
-        expect(res).to.have.status(201);
-        expect(isVerified).to.equal(false);
-        expect(emailSent).to.equal(false);
-        done(err);
-      });
+    const response = await chai.request(server).post('/api/auth/signup').send(user2);
+    const { body: { data } } = response;
+    newlyCreatedUser = { ...data };
+    expect(response).to.have.status(201);
+    expect(data.isVerified).to.equal(false);
+    expect(data.emailSent).to.equal(false);
   });
 });
 describe('GET /api/auth/verify?token', () => {
-  it('should successfully verify an existing user if the token is valid', (done) => {
+  it('should successfully verify an existing user if the token is valid', async () => {
     const { id, firstName, role } = newlyCreatedUser;
     const token = generateToken({ id, firstName, role });
-    chai
-      .request(server)
-      .get(`/api/auth/verify?token=${token}`)
-      .end((err, res) => {
-        const {
-          body: {
-            data: { isVerified }
-          }
-        } = res;
-        expect(res).to.have.status(200);
-        expect(isVerified).to.equal(true);
-        done(err);
-      });
+    const response = await chai.request(server).get(`/api/auth/verify?token=${token}`);
+    const { body: { data: { isVerified } } } = response;
+    expect(response).to.have.status(200);
+    expect(isVerified).to.equal(true);
   });
-  it('should return an error response if verification token is not provided', (done) => {
-    chai
-      .request(server)
-      .get('/api/auth/verify?token=')
-      .end((err, res) => {
-        const {
-          body: { message }
-        } = res;
-        expect(res).to.have.status(400);
-        expect(message).to.equal('Invalid token, verification unsuccessful');
-        done(err);
-      });
+
+  it('should return an error response if verification token is not provided', async () => {
+    const response = await chai.request(server).get('/api/auth/verify?token=');
+    const { body: { message } } = response;
+    expect(response).to.have.status(400);
+    expect(message).to.equal('Invalid token, verification unsuccessful');
   });
-  it('should return an error response if verification token is not valid', (done) => {
-    chai
-      .request(server)
-      .get('/api/auth/verify?token=73783489d.eue4.78')
-      .end((err, res) => {
-        const {
-          body: { message }
-        } = res;
-        expect(res).to.have.status(400);
-        expect(message).to.equal('Invalid token, verification unsuccessful');
-        done(err);
-      });
+
+  it('should return an error response if verification token is not valid', async () => {
+    const response = await chai.request(server).get('/api/auth/verify?token=73783489d.eue4.78');
+    const { body: { message } } = response;
+    expect(response).to.have.status(400);
+    expect(message).to.equal('Invalid token, verification unsuccessful');
   });
-  it('should return an error response if verification token is not valid', (done) => {
+
+  it("should return an error response if the verification link contains details of a user that doesn't exist", async () => {
     const token = generateToken({ id: 49 });
-    chai
-      .request(server)
-      .get(`/api/auth/verify?token=${token}`)
-      .end((err, res) => {
-        const {
-          body: { message }
-        } = res;
-        expect(res).to.have.status(400);
-        expect(message).to.equal('no user found to verify');
-        done(err);
-      });
+    const response = await chai.request(server).get(`/api/auth/verify?token=${token}`);
+    const { body: { message } } = response;
+    expect(response).to.have.status(400);
+    expect(message).to.equal('no user found to verify');
   });
 });
