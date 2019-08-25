5e7937a85aab32e2d4268c9d6bd4b0243068f0a4
Emmanuel Nwankwo <cnwankwom@gmail.com>
2019-08-25 16:11:04 +0100

    feature(middleware):implement reset password middleware

---
+++ src/middlewares/ResetPassword.js
@@ -0,0 +1,31 @@
+import { changePasswordSchema, passwordResetEmailSchema } from '../validation/validators';
+import { ApiResponse } from '../utils/index';
+import Helpers from '../utils/helpers';
+
+const { validate } = Helpers;
+/**
+ * Collection of methods for ResetPassword
+ * @class ResetPassword
+ */
+class ResetPassword {
+  /**
+     * Middleware for password reset form
+     *
+     * @param {Request} req - The request from the endpoint.
+     * @param {Response} res - The response returned by the method.
+     * @param {Next} next
+     * @returns {next} - Continue with the request.
+     * @memberof ResetPassword
+     */
+  static checkParameters(req, res, next) {
+    const result = validate(req.body, (req.body.email) ? passwordResetEmailSchema
+      : changePasswordSchema);
+    if (result.error !== null) {
+      const { message } = result.error.details[0];
+      return res.status(400).json(new ApiResponse(false, 400, message));
+    }
+    next();
+  }
+}
+
+export default ResetPassword;
