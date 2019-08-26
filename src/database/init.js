76b5b6ffd651d36288bc67b68215271c71ac5b6d
Ayodele <daylay92@yahoo.com>
2019-08-26 21:08:21 +0100

    refactor(modify): remove redundant file [Finishes #167733643]

--- src/database/init.js
+++
@@ -1,9 +0,0 @@
-import db from '../models';
-import env from '../config/env-config';
-
-if (env.NODE_ENV !== 'production') {
-  const syncOptions = env.NODE_ENV === 'test' ? { force: true } : {};
-  db.sequelize.sync(syncOptions).then(() => {
-    db.sequelize.close();
-  });
-}
