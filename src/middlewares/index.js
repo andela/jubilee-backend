c67ffb9be1d623d63c404c27f2bef2190319aecd
cloud <ejimchisom@gmail.com>
2019-08-28 15:00:37 +0100

    feature(social login): login through facebook and google

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,8 +1,6 @@
 import ResetPassword from './passwordMiddleware';
 import userMiddleware from './userMiddleware';
-import wrongMock from './social/wrongMock';
-import socialMock from './social/socialMock';
 
 export {
-  ResetPassword, wrongMock, socialMock, userMiddleware
+  ResetPassword, userMiddleware
 };
