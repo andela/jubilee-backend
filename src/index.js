6b087422881b8d75b332d274e263b9d333769633
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-08-28 08:22:47 +0100

    feat(login): add login endpoint
    - wrote tests for the endpoint
    - installed cookie-parser
    - implemented the endpoint
    
    [Delivers #167733645]

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import errorhandler from 'errorhandler';
 import morgan from 'morgan';
 import methodOverride from 'method-override';
+import cookieParser from 'cookie-parser';
 import env from './config/env-config';
 import routes from './routes';
 
@@ -13,6 +14,7 @@ const isProduction = env.NODE_ENV === 'production';
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 
 // Normal express config defaults
 app.use(morgan('dev'));
--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -4,12 +4,13 @@ import { ResetPassword, userMiddleware } from '../../middlewares';
 
 const router = Router();
 const {
-  signUp, verifyEmail, sendResetPasswordEmail, resetPassword, verifyPasswordResetLink
+  signUp, verifyEmail, sendResetPasswordEmail, resetPassword, verifyPasswordResetLink, loginUser,
 } = Auth;
 const { checkParameters } = ResetPassword;
 
 router.post('/signup', userMiddleware.onSignup, signUp);
 router.get('/verify', verifyEmail);
+router.post('/login', loginUser);
 router.post('/reset-password/', checkParameters, sendResetPasswordEmail);
 router.get('/reset-password', verifyPasswordResetLink);
 router.post('/password/reset/:email', checkParameters, resetPassword);
