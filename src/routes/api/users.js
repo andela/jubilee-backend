486f5cb9fc1e620405b76be5a4d90d492e8dc22c
Emmanuel Nwankwo <cnwankwom@gmail.com>
2019-08-29 16:15:17 +0100

    feature(route):add user profile endpoints [Ongoing #167733650]

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import { User } from '../../controllers';
 
 const router = Router();
+
+const { userProfile, updateProfile } = User;
+
+router.get('/profile/:userId', userProfile);
+router.patch('/profile/:userId/update', updateProfile);
 
 export default router;
