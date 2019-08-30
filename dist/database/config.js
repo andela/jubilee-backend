0530b42e946ed3abddc870cf8d419ba4bb8594ba
Emmanuel Nwankwo <cnwankwom@gmail.com>
2019-08-30 22:22:09 +0100

    refactor(dist):remove build folder and files

--- dist/database/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _dotenv = _interopRequireDefault(require("dotenv"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_dotenv.default.config();
