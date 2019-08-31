693888a02f491801ab8c7d560975e11e5b4512bb
Emmanuel Nwankwo <cnwankwom@gmail.com>
2019-08-31 15:02:04 +0100

    refactor(authmiddleware):add authentication method

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,6 @@
 import PasswordMiddleware from './passwordMiddleware';
 import AuthMiddleware from './authMiddleware';
-import UserMiddleware from './userMiddleware';
 
 export {
-  PasswordMiddleware, AuthMiddleware, UserMiddleware
+  PasswordMiddleware, AuthMiddleware
 };
