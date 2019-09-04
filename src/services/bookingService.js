2c1e6b3138b8c21559daf11859d9547dfb39b2db
Segun Ogundipe <davephenoms@gmail.com>
2019-09-04 19:17:03 +0100

    chore(service):add create method to booking service
    [Ongoing #167733675]

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,9 +1,26 @@
+import db from '../models';
 
+const { AccommodationBooking } = db;
 /**
  * A collection of methods that handles the business logic for booking travels
  *
  * @class BookingService
  */
-class BookingService {}
+class BookingService {
+  /**
+   *
+   * Create accommodation booking and saves it in the database
+   * @static
+   * @param {object} booking - booking properties to be saved in the database
+   * @returns {Promise<object | null | string> } an object containing the booking properties that was created
+   * @memberof BookingService
+   */
+  static async createAccBooking(booking) {
+    const { dataValues: newBooking } = await AccommodationBooking.create(
+      booking
+    );
+    return newBooking;
+  }
+}
 
 export default BookingService;
