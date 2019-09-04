73c672fb5426964082b78036ece1dbaaec5bed73
Segun Ogundipe <davephenoms@gmail.com>
2019-09-04 18:05:01 +0100

    chore(test):add test case for successfull accommodation booking
    [Starts #167733675]

---
+++ src/test/booking.spec.js
@@ -0,0 +1,25 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import faker from 'faker';
+import server from '../index';
+
+chai.use(chaiHttp);
+
+describe('Booking Route Endpoints', () => {
+  it('should successfully create an accommodation booking', async () => {
+    const booking = {
+      fullname: faker.name.firstName(),
+			checkIn: new Date();
+			checkOut: new Date();
+    };
+
+    const response = await chai
+      .request(server)
+      .post('/api/booking/accommodation')
+      .send(booking);
+
+    expect(response).to.have.status(201);
+    expect(response.body.data).to.be.a('object');
+    expect(response.body.data).to.be.a('string');
+  });
+});
