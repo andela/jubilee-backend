e403de7fbf6dcb4c21f81801d45aca6d274c9316
dchima <dcnnad@gmail.com>
2019-09-08 10:50:30 +0100

    ch(refactor): refactored roles permission class
    [Finishes #168243791]

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -1,5 +1,6 @@
 import { Helpers } from '../utils';
 import { RoleService } from '../services/index';
+import { callbackify } from 'util';
 
 const {
   verifyToken, errorResponse,
@@ -8,58 +9,25 @@ const {
  * Middleware for input validations
  */
 export default class roleMiddleware {
-
   /**
-     * Middleware method for role permission checks
-     * @param {object} req - The request from the endpoint.
-     * @param {object} res - The response returned by the method.
-     * @param {object} next - the returned values going into the next operation.
-     * @returns {object} - returns an object (error or response).
+     * Middleware method for verifying roles
+     * @param {array} permissions - The permission array.
+     * @returns {function} - returns a function
      */
-  static async verifyCompanyRoles(req, res, next) {
-    try {
-      const { token } = req.cookies;
-      const { permissionId } = req.body;
-      const { id } = verifyToken(token);
-      const { roleId } = await RoleService.getRoles(id);
-      if (permissionId === 5 && roleId === permissionId) {
-        return next();
-      }
-      if (permissionId < 5) {
-        if (roleId <= permissionId) {
+  static verifyRoles(permissions) {
+    return async function foo(req, res, next) {
+      try {
+        const { token } = req.cookies;
+        const { id } = verifyToken(token);
+        const { roleId } = await RoleService.getRoles(id);
+        const permitted = permissions.includes(roleId);
+        if (permitted) {
           return next();
         }
+        return errorResponse(res, { code: 401, message: 'You are an unauthorized user' });
+      } catch (error) {
+        errorResponse(res, { code: 500, message: error.message });
       }
-      return errorResponse(res, { code: 401, message: 'You are an unauthorized user' });
-    } catch (error) {
-      errorResponse(res, { code: 500, message: error.message });
-    }
-  }
-
-  /**
-     * Middleware method for role permission checks on supplier roles
-     * @param {object} req - The request from the endpoint.
-     * @param {object} res - The response returned by the method.
-     * @param {object} next - the returned values going into the next operation.
-     * @returns {object} - returns an object (error or response).
-     */
-  static async verifySupplierRoles(req, res, next) {
-    try {
-      const { token } = req.cookies;
-      const { permissionId } = req.body;
-      const { id } = verifyToken(token);
-      const { roleId } = await RoleService.getRoles(id);
-      if (permissionId === 8 && roleId === permissionId) {
-        return next();
-      }
-      if (permissionId < 8 && permissionId > 5) {
-        if (roleId <= permissionId) {
-          return next();
-        }
-      }
-      return errorResponse(res, { code: 401, message: 'You are an unauthorized user' });
-    } catch (error) {
-      errorResponse(res, { code: 500, message: error.message });
-    }
+    };
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,14 +6,14 @@ import { AuthMiddleware, RoleMiddleware } from '../middlewares';
 const router = Router();
 
 const { updateUserRole } = RoleController;
-const { verifyCompanyRoles } = RoleMiddleware;
+const { verifyCompanyRoles, verifyRoles } = RoleMiddleware;
 const { userProfile, updateProfile } = UserController;
 const { isAuthenticated } = AuthMiddleware;
-const { one } = PermissionId;
+const { superAdmins } = PermissionId;
 
 router.get('/profile/:userId', isAuthenticated, userProfile);
 router.put('/profile/:userId', isAuthenticated, updateProfile);
 
-router.patch('/role', one, verifyCompanyRoles, updateUserRole);
+router.patch('/role', verifyRoles(superAdmins), updateUserRole);
 
 export default router;
