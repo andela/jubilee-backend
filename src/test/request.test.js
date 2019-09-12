f329cf05666e5f1e3b58089b8070ab7ade0725de
cloud <ejimchisom@gmail.com>
2019-09-12 23:07:21 +0100

    feature(approve or reject user travel requests)

--- src/test/request.test.js
+++ src/test/request.test.js
@@ -385,8 +385,8 @@ describe('Request Endpoints', () => {
       requesterId: newlyCreatedUser.id,
       managerId: newlyCreatedCompany.admin.id,
       purpose: 'official',
-      status: 2,
-      tripType: 'round-trip',
+      statusId: 2,
+      tripType: 'Round-Trip',
       origin: 'lagos',
       destination: 'Lagos',
       departureDate: new Date(),
@@ -403,56 +403,25 @@ describe('Request Endpoints', () => {
     expect(response.body.data).to.be.a('object');
     newlyCreatedRequest = response.body.data;
   });
-
-  it('should get all request for a particular user', async () => {
-    const response = await chai
-      .request(server)
-      .get(`/api/users/requests/${newlyCreatedUser.id}`)
-      .set('Cookie', `token=${userToken};`);
-    expect(response).to.have.status(200);
-    expect(response.body.data).to.be.a('array');
-    expect(response.body.status).to.equal('success');
-  });
   it('should get request by id in token and param status', async () => {
     const response = await chai
       .request(server)
-      .get(`/api/users/requests/user/${newlyCreatedRequest.status}`)
-      .set('Cookie', `token=${userToken}`);
+      .get(`/api/users/requests/${newlyCreatedRequest.statusId}`)
+      .set('Cookie', `token=${companyAdminToken}`);
     expect(response).to.have.status(200);
-    expect(response.body.data).to.be.a('array');
     expect(response.body.status).to.equal('success');
   });
 
   it('should throw error if wrong status param is passed', async () => {
     const response = await chai
       .request(server)
-      .get('/api/users/requests/user/approve')
-      .set('Cookie', `token=${userToken}`);
+      .get('/api/users/requests/5')
+      .set('Cookie', `token=${companyAdminToken}`);
     expect(response).to.have.status(400);
-    expect(response.body.error.message).to.equal('Request statusId is required can only be 1, 2, 3 - approved, pending, rejected');
+    expect(response.body.error.message).to.equal('Request statusId can only be values 1, 2, 3 - approved, pending, rejected');
   });
 
-  it('should get all request by admins only', async () => {
-    const response = await chai
-      .request(server)
-      .get('/api/users/requests')
-      .set('Cookie', `token=${companyAdminToken};`);
-    expect(response).to.have.status(200);
-    expect(response.body.data).to.be.a('array');
-    expect(response.body.status).to.equal('success');
-  });
-
-  it('should throw error if not admin', async () => {
-    const response = await chai
-      .request(server)
-      .get('/api/users/requests')
-      .set('Cookie', `token=${userToken}`);
-    expect(response).to.have.status(401);
-    expect(response.body.status).to.equal('fail');
-    expect(response.body.error.message).to.equal('You are an unauthorized user');
-  });
-
-  const newlyRequest = { status: 'approved' };
+  const newlyRequest = { statusId: 1 };
 
   it('should update request by request id in params', async () => {
     const response = await chai
@@ -461,11 +430,21 @@ describe('Request Endpoints', () => {
       .set('Cookie', `token=${companyAdminToken};`)
       .send(newlyRequest);
     expect(response).to.have.status(200);
-    expect(response.body.data).to.be.a('object');
     expect(response.body.status).to.equal('success');
   });
 
-  const wrongRequest = { status: 'approve' };
+  it('should throw error if wrong requestId is specified', async () => {
+    const response = await chai
+      .request(server)
+      .patch('/api/users/requests/hh')
+      .set('Cookie', `token=${companyAdminToken};`)
+      .send(newlyRequest);
+    expect(response).to.have.status(400);
+    expect(response.body.status).to.equal('fail');
+    expect(response.body.error.message).to.equal('Request Id can only be a number');
+  });
+
+  const wrongRequest = { statusId: '6' };
 
   it('should throw error if wrong status update is specified', async () => {
     const response = await chai
@@ -475,8 +454,7 @@ describe('Request Endpoints', () => {
       .send(wrongRequest);
     expect(response).to.have.status(400);
     expect(response.body.status).to.equal('fail');
-    expect(response.body.error).to.be.a('object');
-    expect(response.body.error.message).to.equal('Request statusId is required can only be 1, 2, 3 - approved, pending, rejected');
+    expect(response.body.error.message).to.equal('Request statusId can only be values 1, 2, 3 - approved, pending, rejected');
   });
 
   it('should throw error if wrong request id is specified', async () => {
@@ -488,5 +466,75 @@ describe('Request Endpoints', () => {
     expect(response).to.have.status(404);
     expect(response.body.status).to.equal('fail');
     expect(response.body.error.message).to.equal('updateRequest: No such request');
+  });
+});
+
+
+describe('Request route endpoints', () => {
+  let adminToken;
+  let companyAdminResponse;
+
+  before(async () => {
+    const reqCompany = { body: { ...companyAdmin, email: 'baystef@slack.com', companyName: 'paystack' } };
+
+    const res = {
+      status() {
+        return this;
+      },
+      cookie() {
+        return this;
+      },
+      json(obj) {
+        return obj;
+      }
+    };
+
+    companyAdminResponse = await companySignUp(reqCompany, res);
+    const { data: { signupToken } } = companyAdminResponse;
+    const reqUser = {
+      body: {
+        ...newCompanyUser, email: 'steve@google.com', signupToken, roleId: 5
+      }
+    };
+    const companyUserResponse = await userSignup(reqUser, res);
+    adminToken = companyUserResponse.data.token;
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('GET api/users/requests', () => {
+    it('should return 404 for user with no request yet', async () => {
+      const response = await chai.request(server).get('/api/users/requests').set('Cookie', `token=${adminToken}`);
+      expect(response).to.have.status(404);
+      expect(response.body.error.message).to.be.eql('You have made no request yet');
+    });
+    it('should return a 500 error if something goes wrong while getting the requests', async () => {
+      const req = {
+        body: {}
+      };
+      const mockResponse = () => {
+        const res = {};
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(res);
+        return res;
+      };
+      const res = mockResponse();
+      sinon.stub(RequestService, 'getRequests').throws();
+      await RequestController.getUserRequests(req, res);
+      expect(res.status).to.have.been.calledWith(500);
+    });
+    it('should get a request successfuly', async () => {
+      newRequest.managerId = newlyCreatedCompany.id;
+      const newlyRequest = {
+        ...newRequest,
+        managerId: newlyCreatedCompany.admin.id,
+        requesterId: newlyCreatedUser.id
+      };
+      await Request.create(newlyRequest);
+      const response = await chai.request(server).get('/api/users/requests').set('Cookie', `token=${userToken}`);
+      expect(response).to.have.status(200);
+      expect(response.body.status).to.equal('success');
+    });
   });
 });
