20bb1be4f7eb2b9f9b002e61f582848b7470ed71
Ayodele <daylay92@yahoo.com>
2019-09-13 10:22:35 +0100

    feature(update): update swagger docs [Finishes #167733660]

--- src/test/auth.test.js
+++ src/test/auth.test.js
@@ -154,8 +154,6 @@ describe('Auth route endpoints', () => {
   it('should signup a company and return status 201', async () => {
     const response = await chai.request(server).post('/api/auth/signup/company').send(newCompany);
     companyToken = response.body.data.signupToken;
-    const { body: { data: { admin } } } = response;
-    companyAdmin = { ...admin };
     expect(response).to.have.status(201);
     expect(response.body.status).to.equal('success');
     expect(response.body.data).to.be.a('object');
